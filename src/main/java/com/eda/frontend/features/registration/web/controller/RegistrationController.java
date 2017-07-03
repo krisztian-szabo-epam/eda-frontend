1271a30b1322a31df9bec2c5b05c2b7702f43009
Krisztian Szabo <krisztian_gabor_szabo@epam.com>
2017-07-03 14:11:20 +0200

    Registration Controller added with tested form validation

--- src/main/java/com/eda/frontend/features/registration/web/controller/RegistrationController.java
+++ src/main/java/com/eda/frontend/features/registration/web/controller/RegistrationController.java
@@ -1,16 +1,53 @@
 package com.eda.frontend.features.registration.web.controller;
 
+import com.eda.frontend.features.regisration.facade.UserRegistrationFacade;
+import com.eda.frontend.features.registration.web.domain.UserRegistrationViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/registration")
 public class RegistrationController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    public static final String BASE_TEMPLATE_NAME = "registration";
+
+    @Autowired
+    private UserRegistrationFacade facade;
+
+    @ModelAttribute
+    public void addEmptyModelObject(Model model) {
+        model.addAttribute("data", new UserRegistrationViewModel());
+    }
+
+    @GetMapping
     public String index() {
-        return "registration";
+        return BASE_TEMPLATE_NAME;
+    }
+
+    @PostMapping
+    public String registration(@ModelAttribute("data") @Valid UserRegistrationViewModel viewModel, Errors errors) {
+        if (existPasswords(viewModel) && isNotSamePassword(viewModel)) {
+            errors.rejectValue("samePassword", "password.no.match", "Passwords must be the same!");
+        } else if(! errors.hasErrors()) {
+            facade.send(viewModel);
+        }
+        return BASE_TEMPLATE_NAME;
+    }
+
+    private boolean existPasswords(@ModelAttribute("data") @Valid UserRegistrationViewModel viewModel) {
+        return viewModel.getRePassword() != null && viewModel.getPassword() != null;
+    }
+
+    private boolean isNotSamePassword(@ModelAttribute("data") @Valid UserRegistrationViewModel viewModel) {
+        return ! viewModel.getRePassword().equals(viewModel.getPassword());
     }
 
 }
--- src/test/java/com/eda/frontend/features/registration/web/controller/RegistrationControllerTest.java
+++ src/test/java/com/eda/frontend/features/registration/web/controller/RegistrationControllerTest.java
@@ -1,34 +1,130 @@
 package com.eda.frontend.features.registration.web.controller;
 
+import com.eda.frontend.features.regisration.facade.UserRegistrationFacade;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(RegistrationController.class)
 public class RegistrationControllerTest {
 
-    private static final String BASE_URL = "/registration";
+    private static final String BASE_CONTROLLER_URL = "/registration";
     public static final String BASE_VIEW_NAME = "registration";
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private UserRegistrationFacade facade;
+
     @Test
-    public void index() throws Exception {
-        MockHttpServletRequestBuilder content = get(BASE_URL).accept(MediaType.TEXT_PLAIN);
-        mockMvc.perform(content)
-                .andExpect(status().isOk())
-                .andExpect(view().name(BASE_VIEW_NAME));
+    public void indexShouldLoadThePageWithDefaultTemplate() throws Exception {
+        // GIVEN
+        MockHttpServletRequestBuilder content = get(BASE_CONTROLLER_URL).accept(MediaType.TEXT_PLAIN);
+
+        // WHEN
+        ResultActions result = mockMvc.perform(content);
+
+        // THEN
+        result.andExpect(status().isOk());
+        result.andExpect(view().name(BASE_VIEW_NAME));
     }
 
+    @Test
+    public void registrationWhenEmailAddressIsShortShouldDisplayErrorMessage() throws Exception {
+        // GIVEN
+        MockHttpServletRequestBuilder content = post(BASE_CONTROLLER_URL);
+        content.param("emailAddress", "")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        // WHEN
+        ResultActions result = mockMvc.perform(content);
+
+        // THEN
+        result.andExpect(content().string(Matchers.containsString("Email field is mandatory!")));
+    }
+
+    @Test
+    public void registrationWhenEmailAddressIsNotValidShouldDisplayErrorMessage() throws Exception {
+        // GIVEN
+        MockHttpServletRequestBuilder content = post(BASE_CONTROLLER_URL);
+        content.param("emailAddress", "invalidEmailAddress")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        // WHEN
+        ResultActions result = mockMvc.perform(content);
+
+        // THEN
+        result.andExpect(content().string(Matchers.containsString("Email address is not valid!")));
+    }
+
+    @Test
+    public void registrationWhenPasswordIsNotEqualToRepassworFieldShouldDisplayErrorMessage() throws Exception {
+        // GIVEN
+        MockHttpServletRequestBuilder content = post(BASE_CONTROLLER_URL);
+        content.param("password", "password")
+                .param("rePassword", "notpassword")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        // WHEN
+        ResultActions result = mockMvc.perform(content);
+
+        // THEN
+        result.andExpect(content().string(Matchers.containsString("Passwords must be the same!")));
+    }
+
+    @Test
+    public void registrationWhenPasswordIsShortShouldDisplayErrorMessage() throws Exception {
+        // GIVEN
+        MockHttpServletRequestBuilder content = post(BASE_CONTROLLER_URL);
+        content.param("password", "short")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        // WHEN
+        ResultActions result = mockMvc.perform(content);
+
+        // THEN
+        result.andExpect(content().string(Matchers.containsString("Password must be at least 8 character!")));
+    }
+
+    @Test
+    public void registerWhenUserNameIsShortShouldDisplayErrorMessage() throws Exception {
+        // GIVEN
+        MockHttpServletRequestBuilder content = post(BASE_CONTROLLER_URL);
+        content.param("userName", "short")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        // WHEN
+        ResultActions result = mockMvc.perform(content);
+
+        // THEN
+        result.andExpect(content().string(Matchers.containsString("Username must be between 6 and 32 character!")));
+    }
+
+    @Test
+    public void registerWhenUserNameIsLongShouldDisplayErrorMessage() throws Exception {
+        // GIVEN
+        MockHttpServletRequestBuilder content = post(BASE_CONTROLLER_URL);
+        content.param("userName", "123456789012345678901234567890123")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        // WHEN
+        ResultActions result = mockMvc.perform(content);
+
+        // THEN
+        result.andExpect(content().string(Matchers.containsString("Username must be between 6 and 32 character!")));
+    }
 }
